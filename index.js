0e573f947196af090a16675f73583e2e7add659b
LordShiva <litonnath4888@gmail.com>
2023-11-26 03:39:49 +0600

    Added Surveyor Post and get operation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
@@ -10,8 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.rqtbidh.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.rqtbidh.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,25 +26,50 @@ async function run() {
   try {
     await client.connect();
 
-    const userCollection = client.db('Polling-survey').collection('users')
+    const userCollection = client.db("Polling-survey").collection("users");
+    const reviewCollection = client.db("Polling-survey").collection("review");
+    const surveysCollection = client.db("Polling-survey").collection("surveys");
 
-    app.post('/api/v1/jwt', async(req, res)=>{
-      const user = req.user;
-      const payload = { email: user.email };
-      const token = jwt.sign(payload, jwt_secret, { expiresIn: '365d' });
-      res.send(token)
-    })
-
-    app.post('/api/v1/users', async(req, res)=>{
+    app.post("/api/v1/jwt", async (req, res) => {
       const user = req.body;
-      const result = await userCollection.insertOne(user);
-      res.send(result)
-    })
+      const payload = { email: user.email };
+      const token = jwt.sign(payload, jwt_secret, { expiresIn: "365d" });
+      res.send(token);
+    });
 
-    app.get('/api/v1/users', async(req, res)=>{
-      const result = await userCollection.find().toArray()
+    app.post("/api/v1/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const isExist = await userCollection.findOne(filter);
+      if (isExist) {
+        return res.send({ message: "User already Exist" });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/api/v1/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/api/v1/review", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/api/v1/surveys", async (req, res) => {
+      const survey = req.body;
+      const result = await surveysCollection.insertOne(survey);
+      res.send(result);
+    });
+
+    app.get("/api/v1/surveys/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = {email: email}
+      const result = await surveysCollection.find(query).toArray()
       res.send(result)
-    })
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -55,7 +79,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 app.get("/", (req, res) => {
   res.send("Polling Survey is Running");
 });
